1a09f0e030598fb1c6624002bfdadd054171cdc1
exionn <139649504+exionn@users.noreply.github.com>
2023-07-16 08:22:33 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,19 @@
 export default (client) => {
   client.on("interactionCreate", (interaction) => {
     if (!interaction.isCommand()) return;
-
+    
     if (!interaction.guildId) return;
-
+    const body = client.slashCommands.map(c => console.log(c));
+    /*
     const command = client.slashCommands.get(interaction.commandName);
 
     if (!command) return;
-
+    
     try {
       command.execute(interaction);
     } catch (err) {
       console.log(err);
     }
+    */
   });
 }
